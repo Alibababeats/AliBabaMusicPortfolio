5a62da11ceabc0482222bb1d509f95722a81cb51
Alibababeats <ali.salem.03.15@gmail.com>
2026-01-03 20:08:07 -0500

    added more beats

--- src/lib/beats.ts
+++ src/lib/beats.ts
@@ -17,5 +17,32 @@ export const beats: Beat[] = [
     duration: '3:10',
     audioSrc: '/audio/sensations.mp3',
     gradient: 'from-purple-600 to-pink-600'
+  },
+  {
+    id: '2',
+    title: 'Yusta',
+    bpm: 120,
+    genre: 'Afrobeat',
+    duration: '2:28',
+    audioSrc: '/audio/yusta.mp3',
+    gradient: 'from-green-600 to-yellow-600'
+  },
+  {
+    id: '3',
+    title: 'Spaceglide - Don Toliver Type Beat',
+    bpm: 121,
+    genre: 'Rap/Hip-Hop',
+    duration: '2:38',
+    audioSrc: '/audio/Spaceglide.mp3',
+    gradient: 'from-blue-600 to-purple-600'
+  },
+  {
+    id: '4',
+    title: 'Suspense',
+    bpm: 119,
+    genre: 'Rap/Hip-Hop',
+    duration: '4:20',
+    audioSrc: '/audio/Suspense.wav',
+    gradient: 'from-green-600 to-purple-600'
   }
 ]
